72de479bec915ab1407becdbd482d99e03b25fc6
Maykon Tardoche <120404503+maykontardoche@users.noreply.github.com>
2024-11-28 12:24:13 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,20 @@
-// Script para controlar a interação do sidebarr
+// Script para controlar a interação do sidebar
 const sidebar = document.getElementById('sidebar');
 
 // Função para abrir o sidebar ao passar o mouse
 sidebar.addEventListener('mouseenter', () => {
     sidebar.style.width = '250px';
+    const links = document.querySelectorAll('#sidebar ul li a');
+    links.forEach(link => {
+        link.style.opacity = 1;
+    });
 });
 
 // Função para fechar o sidebar ao tirar o mouse
 sidebar.addEventListener('mouseleave', () => {
     sidebar.style.width = '70px';
+    const links = document.querySelectorAll('#sidebar ul li a');
+    links.forEach(link => {
+        link.style.opacity = 0;
+    });
 });
